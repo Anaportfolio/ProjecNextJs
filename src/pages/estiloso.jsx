7bb7d19c4a66e6ce7b8a72d6767dc4193fd39f26
Anaportfolio <biamaiaribeiro@gmail.com>
2025-04-16 09:57:57 -0300

    CSS Modularizado

---
+++ src/pages/estiloso.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Estiloso_modulo.module.css';
+
+export default function Estiloso(){
+    return(
+        <div className={styles.roxo}>
+            <h1>Estilo usando Módulos </h1>
+        </div>
+    )
+}
